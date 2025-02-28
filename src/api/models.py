b51fc41154fbfa2f51900b74ebcf16cce4e66886
FranRL03 Ruiz <fjavierrule@gmail.com>
2025-02-28 17:53:41 +0000

    add more models

--- src/api/models.py
+++ src/api/models.py
@@ -44,3 +44,68 @@ class BillItems(db.Model):
     price_per_unit = db.Column(db.Float, nullable=False) 
     quantity = db.Column(db.Integer, nullable=False)
     total_price = db.Column(db.Float, nullable=False)
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    following_id = db.Column(db.Integer)
+    follower_id = db.Column(db.Integer)
+
+class Posts(db.Model):
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description = Column(String)
+    body = Column(String)
+    date = Column(Date)
+    image_url = Column(String)
+    user_id = Column(Integer)
+
+class Comments(db.Model):
+    id = Column(Integer, primary_key=True)
+    body = Column(String)
+    user_id = Column(Integer)
+    post_id = Column(Integer)
+
+class Medias(db.Model):
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum('image', 'video', name='media_type'))
+    url = Column(String)
+    post_id = Column(Integer)
+
+class Followers(db.Model):
+    id = Column(Integer, primary_key=True)
+    following_id = Column(Integer)
+    follower_id = Column(Integer)
+
+class CharacterFavorites(db.Model):
+    __tablename__ = 'character_favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    character_id = Column(Integer)
+
+class PlanetFavorites(db.Model):
+    __tablename__ = 'planet_favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)
+    planet_id = Column(Integer)
+
+class Characters(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    height = Column(String)
+    mass = Column(String)
+    hair_color = Column(String)
+    skin_color = Column(String)
+    eye_color = Column(String)
+    birth_year = Column(String)
+    gender = Column(String)
+
+class Planets(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    diameter = Column(String)
+    rotation_period = Column(String)
+    orbital_period = Column(String)
+    gravity = Column(String)
+    population = Column(String)
+    climate = Column(String)
+    terrain = Column(String)
